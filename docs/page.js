cf218ce7a11a4b342ee0ff1264574dc74f290ff2
keman <k754708625@gmail.com>
2022-10-03 10:51:14 +0800

    feat: 添加静态网站首页文件

--- docs/page.js
+++ docs/page.js
@@ -30,8 +30,9 @@ function onDocumentLoad() {
 
 	let path, localizedPath;
 	const pathname = window.location.pathname;
-	const section = /\/(manual|api|examples)\//.exec( pathname )[ 1 ].toString().split( '.html' )[ 0 ];
-	let name = /[\-A-Za-z0-9]+\.html/.exec( pathname ).toString().split( '.html' )[ 0 ];
+	const section = /\/(manual|api|examples)\//.exec(pathname)[1].toString().split('.html')[0];
+	// let name = /[\-A-Za-z0-9]+\.html/.exec( pathname ).toString().split( '.html' )[ 0 ];
+	let name = pathname.split('/')[pathname.split('/').length - 1];
 
 	switch ( section ) {
 
